196b69584c777b9e92ae49d0861afba4150d009d
RodionKrivosheev <112874635+RodionKrivosheev@users.noreply.github.com>
2023-06-05 02:09:53 +0300

    проблема с аннотациями5

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
 import lombok.NonNull;
-import lombok.AllArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
