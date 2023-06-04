a524eb8e8326ffba4f61574dcb4b5f48f67d9622
RodionKrivosheev <112874635+RodionKrivosheev@users.noreply.github.com>
2023-06-05 01:30:57 +0300

    проблема с аннотациями3

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -8,6 +8,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import java.time.LocalDate;
+
 @Data
 public class User {
     @NotNull
