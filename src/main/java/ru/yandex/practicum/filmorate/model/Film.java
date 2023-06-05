172ead33546336c36149047f2370a0acb1a67a8b
RodionKrivosheev <112874635+RodionKrivosheev@users.noreply.github.com>
2023-06-06 00:48:38 +0300

    проблема с аннотациями6

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,7 +3,7 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.*;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+//import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import java.time.LocalDate;
 
