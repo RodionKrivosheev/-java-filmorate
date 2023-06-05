7f50c2b93c50255c5a210e53b5aa68b2e57bfebe
RodionKrivosheev <112874635+RodionKrivosheev@users.noreply.github.com>
2023-06-06 01:10:30 +0300

    проблема с аннотациями6

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,22 +3,24 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.*;
 import javax.validation.constraints.NotBlank;
-//import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import java.time.LocalDate;
 
 @Data
+@AllArgsConstructor
 public class Film {
+    @NotNull
     private int id;
-
-    private final String name;
     @NotBlank
-    private final String description;
-
-    private final LocalDate dateReliese;
-
+    private String name;
+    @NotBlank
+    private String description;
+    @NotNull
+    private LocalDate releaseDate;
+    @NotNull
     @Positive
-    private final int moveTime;
+    private int duration;
 }
 
 
