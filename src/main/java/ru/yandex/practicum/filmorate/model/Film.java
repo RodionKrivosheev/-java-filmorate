86e0dea59f025ef7b1e8a4322d92a90ed76455da
RodionKrivosheev <112874635+RodionKrivosheev@users.noreply.github.com>
2023-06-07 16:30:02 +0300

    1 пуш переработка исключений и старых классов

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -6,22 +6,44 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
 
 @Data
-@AllArgsConstructor
 public class Film {
+    @NonNull
     @NotNull
     private int id;
+    @NonNull
     @NotBlank
     private String name;
+    @NonNull
     @NotBlank
     private String description;
+    @NonNull
     @NotNull
     private LocalDate releaseDate;
+    @NonNull
     @NotNull
     @Positive
     private int duration;
+
+    private Set<Integer> likes = new HashSet<>();
+
+    private int likesCounter;
+
+    public void addLike(Integer id) {
+        likes.add(id);
+        likesCounter++;
+    }
+
+    public void deleteLike(Integer id) {
+        likes.remove(id);
+        likesCounter--;
+    }
 }
+
 
 
 
