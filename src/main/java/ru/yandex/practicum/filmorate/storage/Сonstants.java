5f684a9bd9a3042c50c6e1bea36de3b70462f52f
RodionKrivosheev <112874635+RodionKrivosheev@users.noreply.github.com>
2023-06-08 16:53:21 +0300

    фикс 1

--- src/main/java/ru/yandex/practicum/filmorate/storage/Сonstants.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/Сonstants.java
@@ -3,5 +3,5 @@ package ru.yandex.practicum.filmorate.storage;
 import java.time.LocalDate;
 
 public class constants {
-    static final LocalDate MIN_DATE = LocalDate.of(1895, 12, 28);
+    static final LocalDate minDate = LocalDate.of(1895, 12, 28);
 }
